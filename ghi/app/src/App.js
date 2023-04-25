259dcf7e61ecf6cc5371ec820dc2cb9ba4f822b7
clarisse <clarissenicole3@gmail.com>
2023-04-24 18:44:22 -0700

    manufacturer list complete and working

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
+import ManufacturersList from './ManufacterersList';
 import Nav from './Nav';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="list" element={<ManufacturersList />} />
         </Routes>
       </div>
     </BrowserRouter>
