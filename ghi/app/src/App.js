0917a9295f394b87869469157ce165700b0d13ac
Dylan <Dylanwinn09@gmail.com>
2023-04-24 18:41:52 -0600

    added Manufacturer Form and working link to it in Navbar

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,14 +1,23 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
+import ManuForm from './ManuForm';
 
-function App() {
+function App(props) {
   return (
     <BrowserRouter>
       <Nav />
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
+          {/* <Route path="Manufacturers" element={<Manufacturers />} /> */}
+          <Route path = "manuform">
+            <Route path="new" element={<ManuForm />} />
+          </Route>
+          {/* <Route path="listModels" element={<listModels />} /> */}
+          {/* <Route path="createModel" element={<createModel />} /> */}
+          {/* <Route path="showInventory" element={<showInventory />} /> */}
+          {/* <Route path="createCar" element={<createCar />} /> */}
         </Routes>
       </div>
     </BrowserRouter>
---
+++ ghi/app/src/ManuForm.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+
+function ManuForm() {
+    const [manufacturer, setManufacturer] = useState('');
+
+    const handleManufacturerChange = (event) => {
+        const value = event.target.value;
+        setManufacturer(value);
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const data = {};
+        data.name = manufacturer;
+
+        const manufacturerUrl = 'http://localhost:8100/api/manufacturers/';
+        const fetchConfig = {
+            method: "post",
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        };
+
+        const response = await fetch(manufacturerUrl, fetchConfig);
+        if (response.ok) {
+            const newManufacturer = await response.json();
+            console.log(newManufacturer);
+
+            setManufacturer('');
+        }
+      }
+
+      return (
+        <div className="row">
+          <div className="offset-3 col-6">
+            <div className="shadow p-4 mt-4">
+              <h1>Create a Manufacturer</h1>
+              <form onSubmit={handleSubmit} id="create-manufacturer-form">
+              <div className="form-floating mb-3">
+                  <input onChange={handleManufacturerChange} placeholder="Manufacturer" required type="text" name="manufacturer" id="manufacturer" className="form-control" value={manufacturer}/>
+                  <label htmlFor="model_name">Manufacturer</label>
+                </div>
+                <button className="btn btn-primary">Create!</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      );
+
+}
+
+export default ManuForm;
